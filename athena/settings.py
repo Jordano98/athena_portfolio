90ce79a2699a8833507e37a33eaf9a12b8ed3de3
Jordano98 <mohammad.dehghan218@gmail.com>
2022-11-02 18:48:52 +0330

    static and media file of choosen template is settled
    the next part is view and urls configuration completion

--- athena/settings.py
+++ athena/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'athena.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,6 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR/'static'
+STATICFILES_DIRS=[BASE_DIR/'statics']
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
