f69eba695890315a5ec5f9bf0538602b1089ec61
Jordano98 <mohammad.dehghan218@gmail.com>
2022-12-17 03:26:40 +0330

    modelling part is over successfully

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from portfolio.models import Collection,All_col,Project,Pro_Gallery 
+
+class ProjectAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    list_display = ('pro_name','size','created_date')
+    list_filter = ('pro_name','created_date')
+    search_fields = ['pro_name']
+
+admin.site.register(Project,ProjectAdmin)
+admin.site.register(Pro_Gallery)
+admin.site.register(All_col)
+admin.site.register(Collection)
+
+
 # Register your models here.
--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
+class All_col(models.Model):
+    all_name=models.CharField(max_length=255)
+
+    def __str__(self) :
+        return self.all_name
+
+class Pro_Gallery(models.Model):
+    image = models.ImageField(upload_to='portfolio/', default='./portfolio-1.jpg')
+    image_name=models.CharField(max_length=255,null=True, blank=True)
+    
+    def __str__(self) :
+        return self.image_name
+    class Meta:
+        verbose_name_plural='pictures'
+
+
+class Project(models.Model):
+    pro_name=models.CharField(max_length=255)
+    description = models.TextField()
+    size=models.CharField(max_length=300)
+    gallery = models.ManyToManyField(Pro_Gallery)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self) :
+        return self.pro_name
+
+
+class Collection(models.Model):
+    col_name=models.ForeignKey(All_col,on_delete=models.DO_NOTHING,null=True, blank=True)
+    col_main_name=models.CharField(max_length=255,null=True, blank=True)
+    col_desc = models.TextField()
+    material=models.CharField(max_length=255)
+    date=models.CharField(max_length=255)
+    gallery = models.ManyToManyField(Pro_Gallery)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self) :
+        return self.col_main_name
+
+
 # Create your models here.
