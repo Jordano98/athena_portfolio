fc71bd2c70abc2ca7c1fb8a75308694250c4b850
Jordano98 <mohammad.dehghan218@gmail.com>
2022-12-17 01:25:25 +0330

    making new page named gallery for everyspecific art pieces
    completion of front design until now :))
    now we are gonna work on backend part and medelling the data

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from portfolio.views import portfolio_view
+from portfolio.views import portfolio_view,portfolio_galview
 app_name='portfolio'
 
 urlpatterns=[
-    path('details/',portfolio_view,name='index'),
+    path('details/',portfolio_view,name='details'),
+    path('gallery/',portfolio_galview,name='gallery'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 
 def portfolio_view(request):
     return render(request,'portfolio/portfolio-single-3.html')
+
+def portfolio_galview(request):
+    return render(request,'portfolio/portfolio-gal.html')
 # Create your views here.
